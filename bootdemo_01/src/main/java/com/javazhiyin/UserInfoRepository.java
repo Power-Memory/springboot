28399c48a449dec6aec460bde3a232625f77cbc9
javazhiyin <13732940261@163.com>
2018-07-04 18:38:10 +0800

    springboot-jpa-mysql

---
+++ bootdemo_01/src/main/java/com/javazhiyin/UserInfoRepository.java
@@ -0,0 +1,10 @@
+package com.javazhiyin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by 57783 on 2018/7/4.
+ */
+public interface UserInfoRepository extends JpaRepository<UserInfo,Integer>{
+
+}
